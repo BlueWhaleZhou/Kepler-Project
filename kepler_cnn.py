360a50474cf868630767f23de25ce854dee25c3e
Qinghai <zqhnju241@gmail.com>
2016-03-03 17:43:30 -0500

    Update kepler_cnn.py

--- kepler_cnn.py
+++ kepler_cnn.py
@@ -7,18 +7,18 @@ Get to 0.835 test accuracy after 2 epochs. 100s/epoch on K520 GPU.
 
 from __future__ import print_function
 import numpy as np
-np.random.seed(1337)  # for reproducibility
+#np.random.seed(1337)  # for reproducibility
 
 from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.embeddings import Embedding
+#from keras.layers.embeddings import Embedding
 from keras.layers.convolutional import Convolution1D, MaxPooling1D
-from keras.datasets import imdb
+#from keras.datasets import imdb
 
 
 # set parameters:
-max_features = 5000
+max_features = 50000
 maxlen = 100
 batch_size = 32
 embedding_dims = 100
@@ -28,55 +28,61 @@ hidden_dims = 250
 nb_epoch = 10
 
 print('Loading data...')
-data_file1 = "x-3d4hr_0210_training_nor.csv"
-data_file2 = "x-3d4hr_0210_testing_nor.csv"
-data_file3 = "y-3d4hr_0210_training.csv"
-data_file4 = "y-3d4hr_0210_testing.csv"
+data_file1 = "x_0303_training_new.txt"
+data_file2 = "x_0303_testing_new.txt"
+data_file3 = "y_0303_training.txt"
+data_file4 = "y_0303_testing.txt"
 
 # data loading
-X_train = pd.read_csv(data_file1, delimiter=',', error_bad_lines=False, header=None)
-X_train = X_train.as_matrix()
+X_train = np.loadtxt(data_file1, delimiter=',')
+print(X_train.shape)
+y_train = np.loadtxt(data_file3, delimiter=',')
+print(y_train.shape)
+X_test = np.loadtxt(data_file2, delimiter=',')
+print(X_test.shape)
+y_test = np.loadtxt(data_file4, delimiter=',')
+print(y_test.shape)
 
-y_train = pd.read_csv(data_file3, delimiter=',', error_bad_lines=False, header=None)
-y_train = y_train.as_matrix()
-
-X_test = pd.read_csv(data_file2, delimiter=',', error_bad_lines=False, header=None)
-X_test = X_test.as_matrix()
-
-y_test = pd.read_csv(data_file4, delimiter=',', error_bad_lines=False, header=None)
-y_test = y_test.as_matrix()
-
-print(len(X_train), 'train sequences')
-print(len(X_test), 'test sequences')
-print(y_train)
-print(y_test)
+#print(y_train)
+#print(y_test)
 
 #char = input("...")	
 	
 #print('Pad sequences (samples x time)')
 #X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
 #X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
-print('X_train shape:', X_train.shape)
-print('X_test shape:', X_test.shape)
+#print('X_train shape:', X_train.shape)
+#print('X_test shape:', X_test.shape)
 
 print('Build model...')
 model = Sequential()
 
 # we start off with an efficient embedding layer which maps
 # our vocab indices into embedding_dims dimensions
-model.add(Embedding(max_features, embedding_dims, input_length=maxlen))
-model.add(Dropout(0.25))
+#model.add(Embedding(max_features, embedding_dims, input_length=maxlen))
+#model.add(Dropout(0.25))
 
 # we add a Convolution1D, which will learn nb_filter
 # word group filters of size filter_length:
-model.add(Convolution1D(nb_filter=nb_filter,
-                        filter_length=filter_length,
+model.add(Convolution1D(nb_filter=32,
+                        filter_length=5,
                         border_mode='valid',
                         activation='relu',
                         subsample_length=1))
 # we use standard max pooling (halving the output of the previous layer):
-model.add(MaxPooling1D(pool_length=2))
+#model.add(MaxPooling1D(pool_length=2))
 
+model.add(Convolution1D(nb_filter=64,
+                        filter_length=3,
+                        border_mode='valid',
+                        activation='relu',
+                        subsample_length=1))
+model.add(MaxPooling1D(pool_length=2))
+model.add(Convolution1D(nb_filter=64,
+                        filter_length=3,
+                        border_mode='valid',
+                        activation='relu',
+                        subsample_length=1))
 # We flatten the output of the conv layer,
 # so that we can add a vanilla dense layer:
 model.add(Flatten())
