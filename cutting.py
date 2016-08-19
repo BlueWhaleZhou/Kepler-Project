2ed5303f5879abbd2d6d0eedb688172dec73e8e6
HTDZ <zqhnju241@gmail.com>
2016-08-19 12:19:52 -0400

    changes for original data

--- cutting.py
+++ cutting.py
@@ -1,17 +1,17 @@
+#combinning: all *.fits data
 from astropy.io import fits
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 import os
 import glob
 import math
 
 def loaddata():
-	path1='/home/qinghai/0021_2015Nov12/giant/'
+	path1='/home/qinghai/0021_superEarths/243days/'
 
-	step = 50
+	step = 20
 	name1 = []
-	period = 3
+	period = 243
 	star = dict()
 
 #length estimation
@@ -26,33 +26,34 @@ def loaddata():
 	print (summ)
 #	x = input('...')
 #loading data into matrix
-	for filename in glob.glob(os.path.join(path1, '005780885*.fits')):
+	for filename in glob.glob(os.path.join(path1, '*.fits')):
 		dir_len = len(path1)
-		name_tmp = filename[dir_len:].split('_')
-		star_name = name_tmp[0]
-		num_tmp = int(name_tmp[1][1:3])
-		if star_name in star:
-			star[star_name] = max(star[star_name], num_tmp)
-		else:
-			star[star_name] = num_tmp
-	s = (step, 120589)
-	trainx = np.zeros(s)
-	trainy = np.zeros(120589)
-	il = 0
-	index = 0
-	for key in star:
-		print key
-		time = []
-		flux = []
-		max_num = star[key]
-		for i in xrange(1,max_num+1):
-			if i>=10:		
-				file_name = key+"_q"+str(i)+'.fits'
-			else:
-				file_name = key+"_q0"+str(i)+'.fits'
-			data2 = fits.getdata('/home/qinghai/0021_2015Nov12/giant/'+file_name)
-			time.extend(data2[0])
-			flux.extend(data2[1])
+        name_tmp = filename[dir_len:].split('_')
+        star_name = name_tmp[0]
+        print star_name
+        input('break...')
+        num_tmp = int(name_tmp[1][1:3])
+        if star_name in star:
+            star[star_name] = max(star[star_name], num_tmp)
+        else:
+            star[star_name] = num_tmp
+    s = (step, 200000)
+    trainx = np.zeros(s)
+    trainy = np.zeros(295167)
+    il = 0
+    index = 0
+    for key in star:   
+        time = []
+        flux = []
+        max_num = star[key]
+        for i in xrange(1,max_num+1):
+            if i>=10:		
+                file_name = key+"_q"+str(i)+'.fits'
+            else:
+                file_name = key+"_q0"+str(i)+'.fits'
+            data2 = fits.getdata('/home/qinghai/0021_superEarths/243days/'+file_name)
+            time.extend(data2[0])
+            flux.extend(data2[1])
 		initialtime = time[0] + period
 		k = 0
 		t = 0
@@ -65,19 +66,60 @@ def loaddata():
 		for j in range(int(math.floor(len(time)/step)-1)):
 			fluxtem = flux[initial:initial+step]
 			timetem = time[initial:initial+step]
-			plt.plot(timetem, fluxtem)
-			plt.show()
+#			plt.plot(timetem, fluxtem)
+#			plt.show()
 			trainy[j+index] = 0
 			for m in range(len(transits)):
 				if(time[initial] < transits[m] and transits[m] < time[initial + step]):
 					trainy[j+index] = 1
-#print trainy[j+index]
+#			print trainy[j+index]
 			trainx[:, j+index] = fluxtem
 			initial = initial + step
 		index = index + int(math.floor(len(time)/step)-1)
-	print j + index
-#	x = input('...')
-	p = 'x_0021Nov12_243days.txt'
-	q = 'y_0021Nov12_243days.txt'
+	print j+index
+	p = 'x_superEarth_243days.txt'
+	q = 'y_superEarth_243days.txt'
 	np.savetxt(p, trainx, delimiter=',')
+	np.savetxt(q, trainy, delimiter=',')
+'''
+#main function
+	s=(100,31982)
+	trainx=np.zeros(s)
+	trainy=np.zeros()
+	for i in range(len(name1)):
+		data = fits.getdata(name1[i])
+		print (name1[i], i, i/len(name1))
+		time=data[0]
+		flux=data[1]
+		initialtime = time[0]
+		length=len(time)
+		k = 0
+		t = 1
+		print (time[length-1])
+		while(k<time[length-1]): #locating transits in time
+			k=initialtime+t*period
+			transits.append(k)
+			t=t+1
+		initial = 0
+		for j in range(0, int(length/step)):
+			fluxtemp=flux[initial:initial+step]
+			timetemp=time[initial:initial+step]
+			trainy[j+index] = 0
+			plt.plot(timetemp,fluxtemp)
+			for m in range(len(transits)):
+				if(time[initial]<transits[m] and transits[m]<time[initial+step]):
+                    			trainy[j+index] = 0
+			print(trainy[j+index])
+			plt.show()
+			#char = input("please stop")
+			trainx[:,j+index]=fluxtemp
+			initial = initial+step
+		index = index + int(length/step)
+		#print (index)
+	p = 'x_0021_800_0331.csv'
+	q = 'y_0021_800_0331.csv'
+	np.savetxt(p,trainx,delimiter=",")
+	np.savetxt(q,trainy,delimiter=",")
+'''
+
 loaddata()
---
+++ kepler_cnn_new.py
@@ -0,0 +1,145 @@
+from __future__ import print_function
+import numpy as np
+from numpy import newaxis
+import pandas as pd
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.optimizers import SGD
+from keras.layers.convolutional import Convolution1D, MaxPooling1D, ZeroPadding1D
+import matplotlib.pyplot as plt
+#from sklearn.metrics import roc_curve, auc, roc_auc_score
+
+#THEANO_FLAGS=mode=FAST_RUN,device=gpu2,floatX=float32 python kepler_cnn_new.py
+
+
+# set parameters:
+batch_size = 16
+input_length= 20
+nb_epoch = 400
+
+print('Loading data...')
+file1 = "/home/qinghai/data/0021_superEarths/0021_superEarth_0511_l2_F.txt"
+data1 = np.loadtxt(file1, delimiter=',')
+# data loading
+X_train = data1[:60000, :20, newaxis]
+print(X_train.shape)
+
+y_train = data1[:60000, -1]
+print(y_train.shape)
+
+X_test = data1[60000:, :20, newaxis]
+print(X_test.shape)
+
+y_test = data1[60000:, -1]
+print(y_test.shape)
+
+print('Build model...')
+model = Sequential()
+
+# I add 10 Convolution1D, which will learn nb_filter
+# word group filters of size filter_length:
+#model.add(ZeroPadding1D(padding=1))
+model.add(Convolution1D(nb_filter=128,
+                        filter_length=3,
+                        border_mode='valid',
+                        activation='relu',
+						input_dim=1, 
+						input_length=input_length)) 
+#model.add(ZeroPadding1D(padding=1))                       
+model.add(Convolution1D(nb_filter=128,
+                        filter_length=3,
+                        border_mode='valid',
+                        activation='relu'))
+                        
+
+model.add(Convolution1D(nb_filter=128,
+                        filter_length=3,
+                        border_mode='valid',
+                        activation='relu'))
+
+model.add(Convolution1D(nb_filter=128,
+                        filter_length=3,
+                        border_mode='valid',
+                        activation='relu'))
+                    
+#model.add(Convolution1D(nb_filter=128,
+#						filter_length=3,
+#						border_mode='valid',
+#                      activation='relu'))
+
+#model.add(Convolution1D(nb_filter=128,
+#                       filter_length=3,
+#                       border_mode='valid',
+#                      activation='relu'))
+
+#model.add(Convolution1D(nb_filter=128,
+            #            filter_length=3,
+             #           border_mode='valid',
+              #          activation='relu'))
+
+#model.add(Convolution1D(nb_filter=128,
+#                        filter_length=3,
+#                        border_mode='valid',
+#                        activation='relu'))
+
+#model.add(Convolution1D(nb_filter=128,
+#                        filter_length=3,
+#                        border_mode='valid',
+#                        activation='relu'))
+
+#model.add(Convolution1D(nb_filter=128,
+#                        filter_length=3,
+#                       border_mode='valid',
+#                       activation='relu'))
+
+#model.add(MaxPooling1D(pool_length=2))
+
+#flatten the output of the conv layer
+model.add(Flatten())
+
+#two FC layers
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+#model.add(Dense(128))
+#model.add(Dropout(0.25))
+#model.add(Activation('relu'))
+
+# We project onto a single unit output layer, and squash it with a sigmoid:
+model.add(Dense(1)) 
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adadelta',
+              metrics=['accuracy'])
+model.fit(X_train, y_train, batch_size=batch_size,
+          nb_epoch=nb_epoch, validation_data=(X_test, y_test))
+score, acc = model.evaluate(X_test, y_test, batch_size=batch_size)
+print('Test score:', score)
+print('Test accuracy:', acc)
+
+
+classes = model.predict_classes(X_test, batch_size=batch_size)
+#y_prob = model.predict_proba(X_test, batch_size=batch_size)
+#print(y_prob)
+print(classes)
+'''
+fpr, tpr, thresholds = roc_curve(y_test, y_prob)
+roc_auc = auc(fpr, tpr)
+print (fpr)
+print (tpr)
+print (thresholds)
+print (roc_auc_score(y_test, y_prob))
+print (roc_auc)
+plt.plot(fpr, tpr)
+plt.title('ROC Curve', fontsize=14)
+plt.xlabel('FPR', fontsize=14)
+plt.ylabel('TPR', fontsize=14)
+plt.show()
+np.savetxt('superEarth_prediction.txt', classes, delimiter=',')
+np.savetxt('superEarth_probability.txt', y_prob, delimiter=',')
+json_string = model.to_json()
+open('kepler_cnn_architecture.json', 'w').write(json_string)
+model.save_weights('kepler_cnn_weights.h5')
+'''
