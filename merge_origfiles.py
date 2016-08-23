30c152fc0f973f58695b5d0fd5ee282c81ef0546
Zhou <zqhnju241@gmail.com>
2016-08-23 10:31:47 -0400

    merge all original data 0823 morning

--- merge_origfiles.py
+++ merge_origfiles.py
@@ -24,25 +24,23 @@ i = 1
 while i < len(name):
     filepath_time_temp = '/home/qinghai/testing/time/' + name[i] + '_time.txt'
     filepath_flux_temp = '/home/qinghai/testing/flux/' + name[i] + '_flux.txt'
-    print filepath_time_temp
-    print filepath_flux_temp
     time_matrix_tmp = np.loadtxt(filepath_time_temp, delimiter=',')
     flux_matrix_tmp = np.loadtxt(filepath_flux_temp, delimiter=',')
     time_matrix = np.concatenate((time_matrix, time_matrix_tmp), axis=0)
     flux_matrix = np.concatenate((flux_matrix, flux_matrix_tmp), axis=0)
     i = i + 1
-#print time_matrix.shape
-#print flux_matrix.shape
+print time_matrix.shape
+print flux_matrix.shape
 
-np.savetxt('kepler_orig_testing_i.txt', flux_matrix, delimiter=',')
-flux_file = 'kepler_orig_testing_i.txt'
-flux_dataframe = pd.read_csv(flux_file, delimiter=',', header=None, error_bad_lines=False)
-flux_dataframe_mean = flux_dataframe.mean(axis=1)
-flux_dataframe = flux_dataframe.fillna(flux_dataframe_mean, axis=1)
-flux_dataframe = flux_dataframe.fillna(0, axis=1)
-flux_matrix_norm = preprocessing.normalize(flux_dataframe, norm=l2, axis=1, copy=True)
+
+flux_matrix_T = flux_matrix.transpose()
+flux_matrix_T_dataframe = pd.DataFrame(flux_matrix_T)
+flux_dataframe_mean = flux_matrix_T_dataframe.mean(axis=0)
+flux_dataframe = flux_matrix_T_dataframe.fillna(flux_dataframe_mean, axis=0)
+flux_dataframe = flux_matrix_T_dataframe.fillna(0, axis=0)
+flux_matrix_norm = preprocessing.normalize(flux_dataframe, norm=l2, axis=0, copy=True)
 flux_matrix_f = (flux_matrix_norm - 0.2236) * 1000000
-flux_matrix_f = flux_matrix_f.as_matrix()
+flux_matrix_f = flux_matrix_f.transpose()
 print flux_matrix_f.shape
 print flux_matrix_f
 
