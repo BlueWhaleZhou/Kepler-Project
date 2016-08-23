ad6873d900ff2dfd28ba7cc09ee1a68a6546e0b1
HTDZ <zqhnju241@gmail.com>
2016-08-22 21:45:25 -0400

    before testing 0822

--- merge_origfiles.py
+++ merge_origfiles.py
@@ -9,16 +9,17 @@ path_flux = "/home/qinghai/testing/flux/"
 name = []
 
 for filename in glob.glob(os.path.join(path_flux, '*.txt')):
-    dir_len = len(path_time)
+    dir_len = len(path_flux)
     name_tmp = filename[dir_len:].split('_')
     name.append(name_tmp[0])
 
 print name
+
 #concatenate all time and flux files
 filepath_time_temp = '/home/qinghai/testing/time/' + name[0] + '_time.txt'
-time_matrix = np.loadtxt(filepath_time_temp, delimiter = ',')
+time_matrix = np.loadtxt(filepath_time_temp, delimiter=',')
 filepath_flux_temp = '/home/qinghai/testing/flux/' + name[0] + '_flux.txt'
-flux_matrix = np.loadtxt(filepath_temp, delimiter = ',')
+flux_matrix = np.loadtxt(filepath_time_temp, delimiter=',')
 i = 1
 while i < len(name):
     filepath_time_temp = '/home/qinghai/testing/time/' + name[i] + '_time.txt'
@@ -27,11 +28,16 @@ while i < len(name):
     print filepath_flux_temp
     time_matrix_tmp = np.loadtxt(filepath_time_temp, delimiter=',')
     flux_matrix_tmp = np.loadtxt(filepath_flux_temp, delimiter=',')
-    time_matrix = np.concatenate((time_matrix, time_matrix_tmp), axis = 0)
-    flux_matrix = np.concatenate((flux_matrix, flux_matrix_tmp), axis = 0)
+    time_matrix = np.concatenate((time_matrix, time_matrix_tmp), axis=0)
+    flux_matrix = np.concatenate((flux_matrix, flux_matrix_tmp), axis=0)
     i = i + 1
 print time_matrix.shape
 print flux_matrix.shape
+flux_matrix_norm = preprocessing.normalize(flux_matrix, norm=l2, axis=1, copy=True)
+print flux_matrix_norm.shape
+flux_matrix_f = (flux_matrix_norm - 0.2236) * 1000000
+print flux_matrix_f.shape
+print flux_matrix_f
 
-
+np.savetxt('kepler_orig_testing_0822.txt', flux_matrix_f, delimiter=',')
 
