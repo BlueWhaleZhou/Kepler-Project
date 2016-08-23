3f506f14025aac2939537a155658b45f1bc5d3ef
Zhou <zqhnju241@gmail.com>
2016-08-23 16:00:30 -0400

    fixing flux error

--- merge_origfiles.py
+++ merge_origfiles.py
@@ -5,7 +5,7 @@ from sklearn import preprocessing
 import pandas as pd
 #generate time, flux, star_name in order
 name = []
-star_list = []
+
 path_flux = '/home/qinghai/testing/flux/'
 for filename in glob.glob(os.path.join(path_flux, '*.txt')):
     dir_len = len(path_flux)
@@ -21,31 +21,22 @@ filepath_time_temp = '/home/qinghai/testing/time/' + name[0] + '_time.txt'
 time_matrix = np.loadtxt(filepath_time_temp, delimiter=',')
 filepath_flux_temp = '/home/qinghai/testing/flux/' + name[0] + '_flux.txt'
 flux_matrix = np.loadtxt(filepath_flux_temp, delimiter=',')
-index = 0
-while(index < len(flux_matrix))
-    star_list.append(name[0])
-    index = index + 1
-i = 1
+
 while i < len(name):
     filepath_time_temp = '/home/qinghai/testing/time/' + name[i] + '_time.txt'
     filepath_flux_temp = '/home/qinghai/testing/flux/' + name[i] + '_flux.txt'
     time_matrix_tmp = np.loadtxt(filepath_time_temp, delimiter=',')
     flux_matrix_tmp = np.loadtxt(filepath_flux_temp, delimiter=',')
     length_tem = len(flux_matrix_tmp)
-    j = 0
-    while j < length_tem :
-        star_list.append(name[i])
     time_matrix = np.concatenate((time_matrix, time_matrix_tmp), axis=0)
     flux_matrix = np.concatenate((flux_matrix, flux_matrix_tmp), axis=0)
     i = i + 1
 
 length = len(flux_matrix)
-print star_list.shape
-print star_list
+
 print time_matrix.shape
 print flux_matrix.shape
 
-np.savetxt('star_list.txt', name_list, delimiter=',')
 np.savetxt('kepler_orig_time.txt', time_matrix, delimiter=',')
 flux_matrix_T = flux_matrix.transpose()
 flux_matrix_T_dataframe = pd.DataFrame(flux_matrix_T)
